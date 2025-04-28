f1017a40c19bb789481ab9d35d1d51878379db53
Marcoantonio torres <86016767+marco19972015@users.noreply.github.com>
2025-04-28 15:27:09 -0400

    MARCO-37 : Display Name for user selected
    Created a new component which takes in the name of the user that was selected and displays it using string interpolation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserName: string;
 
-  onSelectUser(event) {
-    console.log(event)
+  onSelectUser(selecteduUserId: string) {
+    let selectedUser = DUMMY_USERS.find(user => user.id === selecteduUserId)
+    this.selectedUserName = selectedUser.name
   }
 }
